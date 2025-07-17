f9992b006e2c4833dd971d681c22ede1d01861bb
HACK3R911 <afanasiev-2004@list.ru>
2025-07-17 21:54:23 +0300

    feat: add WorkerPool struct and constructor

--- pkg/worker/pool.go
+++ pkg/worker/pool.go
@@ -1,1 +1,24 @@
 package worker
+
+import (
+	"15_07_2025/internal/model"
+	"15_07_2025/pkg/repositories"
+	"sync"
+)
+
+type WorkerPool struct {
+	tasks      chan *model.Task
+	storage    *repositories.MemoryStorage
+	wg         sync.WaitGroup
+	maxWorkers int
+	active     int
+	mu         sync.Mutex
+}
+
+func NewWorkerPool(n int, s *repositories.MemoryStorage) *WorkerPool {
+	return &WorkerPool{
+		tasks:      make(chan *model.Task, n),
+		storage:    s,
+		maxWorkers: n,
+	}
+}
