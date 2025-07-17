5823527e11776d698e7fd9fd5c1f1fb2afa25f58
HACK3R911 <afanasiev-2004@list.ru>
2025-07-17 21:50:48 +0300

    feat: create MemoryStorage with mutex protection

--- pkg/repositories/memory.go
+++ pkg/repositories/memory.go
@@ -1,1 +1,17 @@
 package repositories
+
+import (
+	"15_07_2025/internal/model"
+	"sync"
+)
+
+type MemoryStorage struct {
+	tasks map[string]*model.Task
+	mu    sync.RWMutex
+}
+
+func NewMemoryStorage() *MemoryStorage {
+	return &MemoryStorage{
+		tasks: make(map[string]*model.Task),
+	}
+}
