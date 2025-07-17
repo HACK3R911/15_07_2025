a5c24db00352aafcee6473028ae46fbd4e4b939a
HACK3R911 <afanasiev-2004@list.ru>
2025-07-17 21:52:23 +0300

    feat: implement extension parsing utility

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,1 +1,11 @@
 package utils
+
+import "strings"
+
+func ParseExtensions(extStr string) map[string]bool {
+	result := make(map[string]bool)
+	for _, ext := range strings.Split(extStr, ",") {
+		result[strings.ToLower(strings.TrimSpace(ext))] = true
+	}
+	return result
+}
