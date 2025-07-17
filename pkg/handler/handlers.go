c873db7cc6744e1e85ccf8cb2c83e9acdc2abd17
HACK3R911 <afanasiev-2004@list.ru>
2025-07-17 21:43:24 +0300

    feat: implement archive handler service

--- pkg/handler/handlers.go
+++ pkg/handler/handlers.go
@@ -1,1 +1,17 @@
 package handler
+
+import (
+	"15_07_2025/config"
+	"15_07_2025/pkg/repositories"
+	"15_07_2025/pkg/worker"
+)
+
+type Handler struct {
+	storage *repositories.MemoryStorage
+	workers *worker.WorkerPool
+	cfg     *config.Config
+}
+
+func NewArchiveHandler(s *repositories.MemoryStorage, w *worker.WorkerPool, cfg *config.Config) *Handler {
+	return &Handler{s, w, cfg}
+}
