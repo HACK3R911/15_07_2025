2e277f83da45645068a2456e83b16baa001efa82
HACK3R911 <afanasiev-2004@list.ru>
2025-07-17 20:17:17 +0300

    added server with graceful shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,57 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"15_07_2025/config"
+	"15_07_2025/pkg/handler"
+	"15_07_2025/pkg/repositories"
+	"15_07_2025/pkg/utils"
+	"15_07_2025/pkg/worker"
+)
+
+func main() {
+	port := flag.Int("port", 8080, "server port")
+	extensions := flag.String("ext", "pdf,jpg,jpeg,png", "allowed extensions")
+	flag.Parse()
+
+	cfg := config.NewConfig(*port, utils.ParseExtensions(*extensions))
+	memStorage := repositories.NewMemoryStorage()
+	wp := worker.NewWorkerPool(3, memStorage)
+	handler := handler.NewArchiveHandler(memStorage, wp, cfg)
+
+	wp.Start()
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", cfg.Port),
+		Handler: handler,
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		log.Printf("Server started on port %d", cfg.Port)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to start server: %v", err)
+		}
+	}()
+
+	<-done
+	log.Println("Server shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	_ = server.Shutdown(ctx)
+
+	wp.Stop()
+	log.Println("Server exited properly")
 }
