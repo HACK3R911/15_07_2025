73c51ee0707eecaea413e27f30a92d09177b5753
HACK3R911 <afanasiev-2004@list.ru>
2025-07-17 20:12:38 +0300

    added config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,13 @@
 package config
+
+type Config struct {
+	Port        int
+	AllowedExts map[string]bool
+}
+
+func NewConfig(port int, exts map[string]bool) *Config {
+	return &Config{
+		Port:        port,
+		AllowedExts: exts,
+	}
+}
